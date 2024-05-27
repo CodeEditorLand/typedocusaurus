7624d005c18b2d27c5fcee306db6850aed4be994
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-05-27 21:13:27 +0300

    squash!

--- Source/options.ts
+++ Source/options.ts
@@ -1,44 +1,44 @@
-import * as path from "path";
+import * as path from 'path';
 
 import {
-	// @ts-ignore
-	type Application,
-	// @ts-ignore
-	type MixedDeclarationOption,
-	// @ts-ignore
-	ParameterType,
-	// @ts-ignore
-	type StringDeclarationOption,
-	// @ts-ignore
-	TSConfigReader,
-	// @ts-ignore
-	TypeDocReader,
-	// @ts-ignore
-} from "typedoc";
+  // @ts-ignore
+  Application,
+  // @ts-ignore
+  MixedDeclarationOption,
+  // @ts-ignore
+  ParameterType,
+  // @ts-ignore
+  StringDeclarationOption,
+  // @ts-ignore
+  TSConfigReader,
+  // @ts-ignore
+  TypeDocReader,
+  // @ts-ignore
+} from 'typedoc';
 
-import type { PluginOptions, SidebarOptions } from "./types";
+import { PluginOptions, SidebarOptions } from './types';
 
 /**
  * Default plugin options
  */
 const DEFAULT_PLUGIN_OPTIONS: PluginOptions = {
-	id: "default",
-	docsRoot: "docs",
-	out: "api",
-	entryDocument: "index.md",
-	hideInPageTOC: true,
-	hideBreadcrumbs: true,
-	sidebar: {
-		fullNames: false,
-		sidebarFile: "typedoc-sidebar.js",
-		indexLabel: "Table of contents",
-		readmeLabel: "Readme",
-		sidebarPath: "",
-	},
-	plugin: ["none"],
-	outputDirectory: "",
-	siteDir: "",
-	watch: false,
+  id: 'default',
+  docsRoot: 'docs',
+  out: 'api',
+  entryDocument: 'index.md',
+  hideInPageTOC: true,
+  hideBreadcrumbs: true,
+  sidebar: {
+    fullNames: false,
+    sidebarFile: 'typedoc-sidebar.js',
+    indexLabel: 'Table of contents',
+    readmeLabel: 'Readme',
+    sidebarPath: '',
+  },
+  plugin: ['none'],
+  outputDirectory: '',
+  siteDir: '',
+  watch: false,
 };
 
 /**
@@ -46,37 +46,37 @@ const DEFAULT_PLUGIN_OPTIONS: PluginOptions = {
  * @param opts
  */
 export const getOptions = (
-	siteDir: string,
-	opts: Partial<PluginOptions>,
+  siteDir: string,
+  opts: Partial<PluginOptions>,
 ): PluginOptions => {
-	// base options
-	let options = {
-		...DEFAULT_PLUGIN_OPTIONS,
-		...opts,
-	};
-	// sidebar
-	if (opts.sidebar === null) {
-		options = { ...options, sidebar: null };
-	} else {
-		const sidebar = {
-			...DEFAULT_PLUGIN_OPTIONS.sidebar,
-			...opts.sidebar,
-		} as SidebarOptions;
-		options = {
-			...options,
-			sidebar: {
-				...sidebar,
-				sidebarPath: path.resolve(siteDir, sidebar.sidebarFile),
-			},
-		};
-	}
-	// additional
-	options = {
-		...options,
-		siteDir,
-		outputDirectory: path.resolve(siteDir, options.docsRoot, options.out),
-	};
-	return options;
+  // base options
+  let options = {
+    ...DEFAULT_PLUGIN_OPTIONS,
+    ...opts,
+  };
+  // sidebar
+  if (opts.sidebar === null) {
+    options = { ...options, sidebar: null };
+  } else {
+    const sidebar = {
+      ...DEFAULT_PLUGIN_OPTIONS.sidebar,
+      ...opts.sidebar,
+    } as SidebarOptions;
+    options = {
+      ...options,
+      sidebar: {
+        ...sidebar,
+        sidebarPath: path.resolve(siteDir, sidebar.sidebarFile),
+      },
+    };
+  }
+  // additional
+  options = {
+    ...options,
+    siteDir,
+    outputDirectory: path.resolve(siteDir, options.docsRoot, options.out),
+  };
+  return options;
 };
 
 /**
@@ -84,37 +84,37 @@ export const getOptions = (
  * @param app
  */
 export const addOptions = (app: Application) => {
-	// configure deault typedoc options
-	app.options.addReader(new TypeDocReader());
-	app.options.addReader(new TSConfigReader());
+  // configure deault typedoc options
+  app.options.addReader(new TypeDocReader());
+  app.options.addReader(new TSConfigReader());
 
-	// expose plugin options to typedoc so we can access if required
-	app.options.addDeclaration({
-		name: "id",
-	} as StringDeclarationOption);
+  // expose plugin options to typedoc so we can access if required
+  app.options.addDeclaration({
+    name: 'id',
+  } as StringDeclarationOption);
 
-	app.options.addDeclaration({
-		name: "docsRoot",
-	} as StringDeclarationOption);
+  app.options.addDeclaration({
+    name: 'docsRoot',
+  } as StringDeclarationOption);
 
-	app.options.addDeclaration({
-		name: "siteDir",
-	} as MixedDeclarationOption);
+  app.options.addDeclaration({
+    name: 'siteDir',
+  } as MixedDeclarationOption);
 
-	app.options.addDeclaration({
-		name: "outputDirectory",
-	} as StringDeclarationOption);
+  app.options.addDeclaration({
+    name: 'outputDirectory',
+  } as StringDeclarationOption);
 
-	app.options.addDeclaration({
-		name: "globalsTitle",
-	} as StringDeclarationOption);
+  app.options.addDeclaration({
+    name: 'globalsTitle',
+  } as StringDeclarationOption);
 
-	app.options.addDeclaration({
-		name: "readmeTitle",
-	} as StringDeclarationOption);
+  app.options.addDeclaration({
+    name: 'readmeTitle',
+  } as StringDeclarationOption);
 
-	app.options.addDeclaration({
-		name: "sidebar",
-		type: ParameterType.Mixed,
-	} as MixedDeclarationOption);
+  app.options.addDeclaration({
+    name: 'sidebar',
+    type: ParameterType.Mixed,
+  } as MixedDeclarationOption);
 };
